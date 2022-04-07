8f49644cc00ac502f2d5a09ea5e316d694411b15
Kevin Leiva <Kevinomg9@gmail.com>
2022-04-06 21:12:19 -0400

    Final

--- src/components/Aboutme.jsx
+++ src/components/Aboutme.jsx
@@ -6,7 +6,8 @@ import Html from './img/Html.svg'
 import cv from './img/Cv.pdf'
 import { IoLogoJavascript,IoLogoReact,IoLogoNodejs } from "react-icons/io5";
 import { DiMongodb,DiPostgresql,DiFirebase,DiCss3,DiHtml5,DiPython } from "react-icons/di";
-import { FaBootstrap } from "react-icons/fa";  
+import { FaBootstrap } from "react-icons/fa";
+import { SiRedux,SiTypescript } from "react-icons/si";
 
 const Aboutme = () => {
     return (
@@ -38,9 +39,11 @@ const Aboutme = () => {
                            <li className={s.sql}><DiPostgresql></DiPostgresql> Sql</li>
                            <li className={s.fb}><DiFirebase></DiFirebase> Firebase</li>
                            <li className={s.boot}><FaBootstrap></FaBootstrap> Bootstrap</li>
-                           <li className={s.Css}><DiCss3></DiCss3>CSS</li>
-                           <li className={s.Html}><DiHtml5></DiHtml5>Html</li>
-                           <li className={s.Python}><DiPython></DiPython>Python</li>
+                           <li className={s.Css}><DiCss3></DiCss3> CSS</li>
+                           <li className={s.Html}><DiHtml5></DiHtml5> Html</li>
+                           <li className={s.Python}><DiPython></DiPython> Python</li>
+                           <li className={s.Redux}><SiRedux></SiRedux> Redux</li>
+                           <li className={s.Type}><SiTypescript></SiTypescript> TypeScript</li>
                        </ul>
                     </div>
                     </AnimationOnScroll>
--- src/components/Proyects.jsx
+++ src/components/Proyects.jsx
@@ -1,19 +1,52 @@
 import React from 'react'
+import "animate.css/animate.min.css"; 
+import { AnimationOnScroll } from 'react-animation-on-scroll';
 import s from './styles/Proyects.module.css'
+import minPoke from'./img/PokeapiMin.png'
+import minCup from'./img/Cup.jpg'
+import minBresh from'./img/minBresh.jpg'
+import { BsYoutube,BsGithub } from "react-icons/bs";  
 
 const Proyects = () => {
   return (
+    
     <div className={s.proyectContainer}>
-        <div>
-            <h1>Hola</h1>
-            <h1>Hola</h1>
-            <h1>Hola</h1>
-            <h1>Hola</h1>
-            <h1>Hola</h1>
-            <h1>Hola</h1>
-            <h1>Hola</h1>
-            <h1>Hola</h1>
+      <AnimationOnScroll animateIn="animate__rotateInUpLeft" >
+        <div className={s.proyects}>
+          <h1>My principal Proyects</h1>
+          <div className={s.cards}>
+            <div className={s.card}>
+              <img className={s.img} src={minPoke} />
+              <div className={s.text}>
+                <h1>Pokeapi</h1>
+                <div className={s.link}>
+                <a className={s.Git} href><BsGithub></BsGithub></a>
+                <a className={s.yt} href="https://youtu.be/_5UAnJd3Jto"><BsYoutube></BsYoutube></a>
+                </div>
+              </div>
+              </div>
+            <div className={s.card}>
+            <img className={s.img} src={minCup} />
+              <div className={s.text}>
+                <h1>Cupcake</h1>
+                <div className={s.link}>
+                <a className={s.Git} href="https://github.com/Kevinekisde/Tienda-Cupcake-React"><BsGithub></BsGithub></a>
+                <a className={s.yt} href="https://youtu.be/Qb6KKPyPG4s"><BsYoutube></BsYoutube></a>
+                </div>
+              </div>
+              </div>
+            <div className={s.card}>
+            <img className={s.img} src={minBresh} />
+              <div className={s.text}>
+                <h1>Bresh</h1>
+                <div className={s.link}>
+                <a className={s.yt} href="https://youtu.be/KCdw72Od4pA"><BsYoutube></BsYoutube></a>
+                </div>
+              </div>
+              </div>
+          </div>
         </div>
+    </AnimationOnScroll>
     </div>
   )
 }
