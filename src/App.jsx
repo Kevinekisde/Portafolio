74341e556686e76a4d81da097feb53e97942d59f
Kevin Leiva <Kevinomg9@gmail.com>
2022-04-10 20:50:25 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <BrowserRouter>
     <Routes>  
-    <Route path="/" element={<Home/>}/>
+    <Route path="" element={<Home/>}/>
     <Route path="/form" element={<Form/>}></Route>
     </Routes>
     </BrowserRouter>
