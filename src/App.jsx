a225e0910ce6b63faba0f5094b234deba0d79651
Kevin Leiva <Kevinomg9@gmail.com>
2022-04-10 20:54:34 -0400

    a

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <BrowserRouter>
     <Routes>  
     <Route path="/" element={<Home/>}/>
-    <Route path="form" element={<Form/>}></Route>
+    <Route path="/form" element={<Form/>}></Route>
     </Routes>
     </BrowserRouter>
   )
