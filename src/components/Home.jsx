3544e9bf46d82f3f73196b961990ab00f6093fdc
Kevin Leiva <Kevinomg9@gmail.com>
2022-04-10 21:29:39 -0400

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -22,7 +22,7 @@ const Home = () => {
             <li><a href="https://github.com/Kevinekisde"><BsGithub></BsGithub></a></li>
             <li><a href="https://www.linkedin.com/in/kevin-leiva-98208a184/"><BsLinkedin></BsLinkedin></a></li>
             <li><a href="https://wa.me/939418935"><BsWhatsapp></BsWhatsapp></a></li>
-            <NavLink to="/forms"><SiGmail></SiGmail></NavLink >
+            <NavLink to="/form"><SiGmail></SiGmail></NavLink >
           </ul>
         </div>
         <img className={s.wave} src={wave}></img>
